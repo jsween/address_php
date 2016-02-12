dddda14d78873515faf0c2bdd8969fa66423d28c
Jon Sweeney <jonsweeney26@gmail.com>
2016-02-12 09:32:01 -0800

    add minor css and source for address class for app.php

--- app/app.php
+++ app/app.php
@@ -1,6 +1,7 @@
 <?php
     require_once __DIR__."/../vendor/autoload.php";
     require_once __DIR__."/../src/Contact.php";
+    require_once __DIR__."/../src/Address.php";
 
     session_start();
 
