5245726da411b26d63099d76e4fb27bdb05e43b2
Jon Sweeney <jonsweeney26@gmail.com>
2016-02-12 08:49:52 -0800

    add logic to app.php to load created contact page

--- app/app.php
+++ app/app.php
@@ -12,9 +12,15 @@
     $app->register(new Silex\Provider\TwigServiceProvider(), array(
         'twig.path' => __DIR__.'/../views'
     ));
-
+    //home page
     $app->get("/", function() use ($app) {
         return $app['twig']->render('address_book.html.twig', array('contacts' => Contact::getAll()));
+    });
+    //created a contact page
+    $app->post("/create_contact", function() use ($app) {
+        $contact = new Contact($_POST['first_name'])));
+        $contact->save();
+        return $app['twig']->render('created_contact.html.twig', array('newcontact' => $contact));
     });
 
     return $app;
